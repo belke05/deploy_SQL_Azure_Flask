b7388eedffbedc10c024b31c90c6b4450b76de73
belke05 <you@example.com>
2019-12-30 16:47:17 +0100

    first commit

--- manage.py
+++ manage.py
@@ -1,9 +1,3 @@
-
-# manage our application
-# but also create database
-# migrations.Indeed, each time you make
-# changes, you will need to migrate your
-# database. So let's learn to do the thingimport os
 import os
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
@@ -14,6 +8,7 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
+# so you can do python manage.py db init upgrade or migrate 
 
 if __name__ == '__main__':
     manager.run()
